054b99a60266b71cdbb37d75d4efff3f08cffc6b
Panuvit Suckchuai <panuvit1165@gmail.com>
2022-08-05 01:30:48 +0700

    create loop for print hello (vertical)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ const word = 'Hello';
 function App() {
   return (
     <div>
-      Array.from(word).map((c, i) ={'>'} <CharacterCard value={c} key={i} />)
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
     </div>
   );
 }
