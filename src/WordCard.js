15944546379280525abf42d7a9bc4efeecff7375
Panuvit Suckchuai <panuvit1165@gmail.com>
2022-08-05 13:53:56 +0700

    create WordCard.js

---
+++ src/WordCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+export default function WordCard(props) {
+    return (
+        <div>{Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>)}
+        </div>
+    );
+}
